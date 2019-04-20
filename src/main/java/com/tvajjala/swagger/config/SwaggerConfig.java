dc9c6d93ca97b42d949a4c1ad57c2872354ba5a2
ThirupathiReddy Vajjala <tvajjala@gmail.com>
2019-04-20 18:12:53 -0400

    Swagger UI integration with spring boot

---
+++ src/main/java/com/tvajjala/swagger/config/SwaggerConfig.java
@@ -0,0 +1,48 @@
+package com.tvajjala.swagger.config;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * Swagger UI project configuration to render, clean API documentation without using springfox annotations
+ *
+ * @author ThirupathiReddy Vajjala
+ */
+
+@Configuration
+public class SwaggerConfig implements WebMvcConfigurer {
+
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(SwaggerConfig.class);
+
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        LOGGER.info("Adding resource handlers to the swagger UI");
+        registry.addResourceHandler("swagger-ui.html").addResourceLocations("classpath:/swagger-ui/swagger-ui.html");
+        registry.addResourceHandler("api/**").addResourceLocations("classpath:/api-doc/");
+        registry.addResourceHandler("*.css").addResourceLocations("classpath:/swagger-ui/");
+        registry.addResourceHandler("*.js").addResourceLocations("classpath:/swagger-ui/");
+        registry.addResourceHandler("images/**").addResourceLocations("classpath:/images/");
+
+    }
+
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        LOGGER.info("Enabling Cross Origin Resource Sharing  ");
+        registry.addMapping("/*");// all the end-points now can be accessible from other domains
+    }
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addRedirectViewController("/", "swagger-ui.html");
+    }
+
+
+}
---
+++ src/test/java/com/tvajjala/swagger/SpringBootSwaggerUiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.tvajjala.swagger;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootSwaggerUiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
